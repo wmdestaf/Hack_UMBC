c84729627261ef97cf6e744dbc059756d59950d1
Will DeStaffan <w55@umbc.edu>
2021-10-30 03:51:40 -0400

    added basic IO

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
 import tkinter as tk
+from tkinter import filedialog as fd
 from tkinter import *
 from tkinter import ttk
+from tkinter import messagebox
+from os.path import exists
 import copy
 import math
     
@@ -21,72 +24,74 @@ class Cell:
     def get_gcol(self):
         return self.possible_colors[self.color]
     
+    def seralize(self):
+        #This will become much more complicated.
+        return str(self.color)
+        
+    def deserialize(self,x):
+        #ditto
+        self.color = int(x)
+        
+    
     def __repr__(self):
         return ",".join(map(str,self.__dict__.keys())) + "\n" + ",".join(map(str,self.__dict__.values()))
    
-def on_cell_click(x,y,r_id,off): #this is horrific garbage use of a 'factory' but I couldn't fit it in a lambda
+def on_cell_click(x,y,r_id,off):
     def internal(ignore):
-        global grid, canv
+        global grid, canv, root, saved
         obj = grid[x][y]
         obj.chg_gcol(off) #change the logical representation
         canv.itemconfig(r_id, fill = obj.get_gcol())      
         canv.update()
+        saved = False
+        root.title(root.title() + ("*" if root.title()[-1] != '*' else ''))
     return internal
    
-def generate_grid():
-    global canv, grid, total_sz, onscreen, mode
-    
-    #Clear any bindings that previously existed
+def clean_grid():
+    global grid, total_sz
     for x in range(total_sz[0]):
         for y in range(total_sz[1]):
             canv.delete(grid[x][y].gid)
+   
+def generate_grid(which_mode):
+    global canv, grid, total_sz, onscreen, mode
+    
+    clean_grid() #Clear any bindings that previously existed
+    
     
     cwidth = canv.winfo_width()
     cheight = canv.winfo_height()
     
     #Render the grid orthogonally
-    dx = cwidth/onscreen[0]
+    
     dy = cheight/onscreen[1]
     
-    for x in range(total_sz[0]):
-        for y in range(total_sz[1]):
-            r_id = canv.create_rectangle(dx*x,dy*y,dx*(x+1),dy*(y+1), fill = grid[x][y].get_gcol()) 
-            canv.tag_bind(r_id, "<Button-1>", on_cell_click(x,y,r_id,1))
-            canv.tag_bind(r_id, "<Button-3>", on_cell_click(x,y,r_id,-1))
-            grid[x][y].gid = r_id
-            
-    mode = "ORTHOGONAL"
-    
-def generate_grid_i():
-    global canv, grid, total_sz, onscreen, mode
-    
-    #Clear any bindings that previously existed
-    for x in range(total_sz[0]):
-        for y in range(total_sz[1]):
-            canv.delete(grid[x][y].gid)
-    
-    cwidth = canv.winfo_width()
-    cheight = canv.winfo_height()
-    
-    dx = cwidth/onscreen[0] * math.sqrt(2) #!
-    dy = cheight/onscreen[1]
-    
-    for x in range(total_sz[0]):
-        for y in range(total_sz[1]):  
-            x1 = (dx*x*0.5) + (0.0*dx) + (0.5*y*dx)
-            y1 = (dy*0.5*y) + (0.5*dy) - (0.5*x*dy)
-            x2 = x1 + 0.5*dx
-            y2 = y1 - 0.5*dy
-            x3 = x1 + 1.0*dx
-            y3 = y1
-            x4 = x2
-            y4 = y1 + 0.5*dy
-            r_id = canv.create_polygon([x1,y1,x2,y2,x3,y3,x4,y4], fill = grid[x][y].get_gcol(),outline='black') 
-            canv.tag_bind(r_id, "<Button-1>", on_cell_click(x,y,r_id,1))
-            canv.tag_bind(r_id, "<Button-3>", on_cell_click(x,y,r_id,-1))
-            grid[x][y].gid = r_id
-    
-    mode = "ISOMETRIC"
+    if which_mode == "ORTHOGONAL":
+        dx = cwidth/onscreen[0]
+        for x in range(total_sz[0]):
+            for y in range(total_sz[1]):
+                r_id = canv.create_rectangle(dx*x,dy*y,dx*(x+1),dy*(y+1), fill = grid[x][y].get_gcol()) 
+                canv.tag_bind(r_id, "<Button-1>", on_cell_click(x,y,r_id,1))
+                canv.tag_bind(r_id, "<Button-3>", on_cell_click(x,y,r_id,-1))
+                grid[x][y].gid = r_id
+        mode = "ORTHOGONAL"
+    else: #ISOMETRIC
+        dx = cwidth/onscreen[0] * math.sqrt(2) #!
+        for x in range(total_sz[0]):
+            for y in range(total_sz[1]):  
+                x1 = (dx*x*0.5) + (0.0*dx) + (0.5*y*dx)
+                y1 = (dy*0.5*y) + (0.5*dy) - (0.5*x*dy)
+                x2 = x1 + 0.5*dx
+                y2 = y1 - 0.5*dy
+                x3 = x1 + 1.0*dx
+                y3 = y1
+                x4 = x2
+                y4 = y1 + 0.5*dy
+                r_id = canv.create_polygon([x1,y1,x2,y2,x3,y3,x4,y4], fill = grid[x][y].get_gcol(),outline='black') 
+                canv.tag_bind(r_id, "<Button-1>", on_cell_click(x,y,r_id,1))
+                canv.tag_bind(r_id, "<Button-3>", on_cell_click(x,y,r_id,-1))
+                grid[x][y].gid = r_id
+        mode = "ISOMETRIC"
     
 def game_loop():
     global canv, grid, keydowns
@@ -159,7 +164,7 @@ def center_screen():
     canv.update()
     
 def new():
-    global grid, onscreen, base_onscreen, total_sz, base_scrollspeed
+    global grid, onscreen, base_onscreen, total_sz, base_scrollspeed, mode, curfile, root
     #Make logical board
     onscreen = [6,6]
     base_onscreen = onscreen
@@ -168,13 +173,17 @@ def new():
     grid[4][4].color = 1
     grid[4][5].color = 3
     grid[5][5].color = 2
-    generate_grid() #Make graphical board
+    
+    generate_grid(mode) #Make graphical board
 
     canv["xscrollincrement"] = 2*onscreen[0]
     canv["yscrollincrement"] = 2*onscreen[1]
     #move the camera to the center of the canvas
     center_screen()
     canv.update()
+    
+    curfile = "*Untitled*"
+    root.title("RPG: " + curfile)
     
 def zoom(n):
     global canv, total_sz, onscreen, base_onscreen, mode
@@ -184,9 +193,87 @@ def zoom(n):
         return #Zoom too close, divide by zero. Zoom too far, centering becomes annoying to calculate for.
     onscreen[0] += n
     onscreen[1] += n
-    generate_grid() if mode == "ORTHOGONAL" else generate_grid_i()  #Costly but necessary to rebind click->action areas within TK. 
+    generate_grid(mode) #Costly but necessary to rebind click->action areas within TK. 
     canv.update()
         
+def load_file():
+    global root, saved, onscreen, base_onscreen, total_sz, grid, canv
+    #Construct the logical grid
+    filename = fd.askopenfilename(filetypes=[("HackUMBC RPG Engine File",".rpg")])
+    if not filename:
+        messagebox.showinfo('Error', 'Please provide an appropriate file.')
+        return
+    try:
+        f = open(filename,"r")
+    except:
+        messagebox.showinfo('Error', 'Could not open ' + filename)
+        return
+    
+    #try:
+    with f:
+        _total_sz = list(map(int,f.readline().strip().split(",")))
+        _onscreen = list(map(int,f.readline().strip().split(",")))
+        _grid     = [[Cell(0,canv,x,y) for x in range(_total_sz[0])] for y in range(_total_sz[1])]
+        for y in range(_total_sz[1]): #used up all my brainpower and forgot how to do this as a list comp
+            line_serial = f.readline().strip().split(",")
+            for x, elem in enumerate(line_serial):
+                _grid[x][y].deserialize(elem)
+    except Exception as e:
+        messagebox.showinfo('Error', 'The file is unsupported or corrupt.')
+        return
+        
+    total_sz = _total_sz
+    onscreen = _onscreen
+    base_onscreen = copy.deepcopy(onscreen)
+    grid = _grid #SLOW
+        
+    saved = True
+    curfile = filename
+    root.title("RPG: " + curfile)
+    
+    clean_grid()
+    generate_grid(mode)
+    center_screen()
+    canv.update()
+    
+def save_file():
+    global root, curfile, saved
+    
+    if curfile == "*Untitled*":    
+       return save_f_as()
+    if not attempt_write(curfile):
+        messagebox.showinfo('Error', 'Could not write to ' + curfile)
+        return
+        
+    root.title("RPG: " + curfile) #important, removes asterisk
+    saved = True
+    
+    
+def save_f_as():
+    global root, curfile, total_sz, onscreen
+    filename = fd.asksaveasfilename(initialfile="Untitled.rpg",defaultextension=".rpg",filetypes=[("HackUMBC RPG Engine File",".rpg")])
+    if not filename:
+        messagebox.showinfo('Error', 'Please enter an appropriate name.')
+        return
+    if not attempt_write(filename):
+        messagebox.showinfo('Error', 'Could not write to ' + filename)
+        return
+        
+    saved = True
+    curfile = filename
+    root.title("RPG: " + curfile)
+        
+def attempt_write(filename):
+    try:
+        with open(filename,"w") as f:
+            f.write(",".join(map(str,total_sz)) + "\n")
+            f.write(",".join(map(str,onscreen)) + "\n")
+            for x in range(total_sz[0]):
+                for y in range(total_sz[1]):    
+                    f.write(grid[x][y].seralize() + ("," if y != total_sz[1] - 1 else "\n"))
+    except EnvironmentError:
+        return False
+    return True
     
 if __name__ == "__main__":
     s_width,s_height = 500,500
@@ -206,14 +293,18 @@ if __name__ == "__main__":
     filemenu = Menu(menubar, tearoff=0)
     filemenu.add_command(label="New", command=new, accelerator="Ctrl-N")
     root.bind_all("<Control-n>", lambda k: new()) #needed, as otherwise tries to pass new a param if just given raw FP
-    filemenu.add_command(label="Open", command=donothing,accelerator="Ctrl-O")
-    filemenu.add_command(label="Save", command=donothing,accelerator="Ctrl-S")
+    filemenu.add_command(label="Open", command=load_file,accelerator="Ctrl-O")
+    root.bind_all("<Control-o>", lambda k: load_file())
+    filemenu.add_command(label="Save", command=save_file,accelerator="Ctrl-S")
+    root.bind_all("<Control-s>", lambda k: save_file())
+    filemenu.add_command(label="Save As...", command=save_f_as,accelerator="Ctrl-Alt-S")
+    root.bind_all("<Control-Alt-s>", lambda k: save_f_as())
     filemenu.add_separator()
     filemenu.add_command(label="Exit", command=root.quit)
     menubar.add_cascade(label="File", menu=filemenu)
 
     viewmenu = Menu(menubar, tearoff=0)
-    viewmenu.add_command(label="Center",     command=center_screen,    accelerator="(C)"   )
+    viewmenu.add_command(label="Center",     command=center_screen,    accelerator="C"   )
     viewmenu.add_command(label="Zoom In",    command=lambda: zoom(-1), accelerator="Ctrl +")
     root.bind_all("<Control-plus>",  lambda k: zoom(-1))
     root.bind_all("<Control-equal>", lambda k: zoom(-1)) #!!!
@@ -222,16 +313,19 @@ if __name__ == "__main__":
     viewmenu.add_command(label="Reset Zoom", command=lambda: zoom(0),  accelerator="Ctrl+0")
     root.bind_all("<Control-0>",     lambda k: zoom(0))
     viewmenu.add_separator()
-    viewmenu.add_command(label="Orthogonal", command=generate_grid,   accelerator="Ctrl+Alt+Shift+O")
-    root.bind_all("<Control-Alt-Shift-O>", lambda k: generate_grid())
-    viewmenu.add_command(label="Isometric",  command=generate_grid_i, accelerator="Ctrl+Alt+Shift+I")
-    root.bind_all("<Control-Alt-Shift-I>", lambda k: generate_grid_i())
+    viewmenu.add_command(label="Orthogonal", command=lambda: generate_grid("ORTHOGONAL"), accelerator="Ctrl+Shift+O")
+    root.bind_all("<Control-Shift-O>", lambda k: generate_grid("ORTHOGONAL"))
+    viewmenu.add_command(label="Isometric",  command=lambda: generate_grid("ISOMETRIC"), accelerator="Ctrl+Shift+I")
+    root.bind_all("<Control-Shift-I>", lambda k: generate_grid("ISOMETRIC"))
     menubar.add_cascade(label="View", menu=viewmenu)
 
     root.config(menu=menubar)
     
+    mode="ORTHOGONAL"
     new() #make fresh board
     keydowns = [False for i in range(4)] #LEFT UP RIGHT DOWN
+    
+    saved = False #IO
     
     game_loop()
     root.mainloop()
