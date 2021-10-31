9c046912893abc186f161074fdb85eed607c003c
Will DeStaffan <w55@umbc.edu>
2021-10-31 04:15:10 -0400

    tile placement

--- src/main.py
+++ src/main.py
@@ -1,22 +1,73 @@
 import tkinter as tk
 from tkinter import filedialog as fd
-from tkinter import *
+#from tkinter import *
 from tkinter import ttk
 from tkinter import messagebox
+from PIL import Image, ImageTk
 from os.path import exists
 import copy
 import math
 import collections
     
 class Cell:
-    def __init__(self, color,canv,x,y):
+    def __init__(self,canv,x,y,color=0):
         self.canv = canv
-        self.color = color
+        self.img = None
+        self.img_id = -1
         self.x=x
         self.y=y
-        self.color = 0
+        self.color = color
         self.possible_colors = ['white','red','pink','blue']
         self.gid = None
+    
+    def change_pretty(self):
+        global canv
+        self.color ^= 1
+        canv.itemconfigure(self.gid, fill=self.get_gcol())
+        canv.update()
+    
+    def fireImageChangeEvent(self, erase):
+        global active_tile, canv
+        
+        if not active_tile and not erase:
+            for i in range(1,5):
+                canv.after(50 * i, lambda: self.change_pretty())
+            
+            onscreen_cell = canv.coords(self.gid)
+            print(onscreen_cell)
+            return
+        
+        if erase: #deletion event
+            canv.delete(self.img_id)
+            self.img = None
+            self.img_id = -1
+        else: #creation event
+            if self.img: #remove any image which is already there
+                canv.delete(self.img_id)
+            self.img = active_tile
+            self.fireImageScaleEvent()
+                
+            #TODO: does not account for images at x:y != 2:1
+            onscreen_cell = canv.coords(self.gid)
+            
+            print(onscreen_cell)
+            draw_x = onscreen_cell[2] #leftest point of the trapezoid
+            draw_y = onscreen_cell[5] #highest point of the trapezoid
+            
+            self.img_id = canv.create_image(draw_x,draw_y,image=self.img)
+            canv.itemconfig(self.img_id, state=tk.DISABLED)
+            
+        canv.update()
+            
+    def fireImageScaleEvent(self):
+        global mode, canv
+        
+        if not self.img:
+            return
+            
+        dx, dy = disp_cell_size_debug(False)
+        pil_img = ImageTk.getimage(self.img)
+        self.img = ImageTk.PhotoImage(pil_img.resize((int(dx),int(dy)), Image.ANTIALIAS))
     
     def chg_gcol(self,off):
         self.color += off
@@ -37,12 +88,14 @@ class Cell:
     def __repr__(self):
         return ",".join(map(str,self.__dict__.keys())) + "\n" + ",".join(map(str,self.__dict__.values()))
    
-def on_cell_click(x,y,r_id,off):
+def on_cell_click_factory(x,y,r_id,erase):
     def internal(ignore):
-        global grid, canv, root, saved
+        global grid, canv, root, saved, active_tile
         obj = grid[x][y]
-        obj.chg_gcol(off) #change the logical representation
-        canv.itemconfig(r_id, fill = obj.get_gcol())      
+
+        #if active_tile or erase: #holy antipattern
+        obj.fireImageChangeEvent(erase)
+        
         canv.update()
         saved = False
         root.title(root.title() + ("*" if root.title()[-1] != '*' else ''))
@@ -58,13 +111,9 @@ def generate_grid(which_mode):
     global canv, grid, total_sz, onscreen, mode
     
     clean_grid() #Clear any bindings that previously existed
-    
-    
     cwidth = canv.winfo_width()
     cheight = canv.winfo_height()
-    
-    #Render the grid orthogonally
-    
+
     dy = cheight/onscreen[1]
     
     if which_mode == "ORTHOGONAL":
@@ -72,8 +121,8 @@ def generate_grid(which_mode):
         for x in range(total_sz[0]):
             for y in range(total_sz[1]):
                 r_id = canv.create_rectangle(dx*x,dy*y,dx*(x+1),dy*(y+1), fill = grid[x][y].get_gcol()) 
-                canv.tag_bind(r_id, "<Button-1>", on_cell_click(x,y,r_id,1))
-                canv.tag_bind(r_id, "<Button-3>", on_cell_click(x,y,r_id,-1))
+                canv.tag_bind(r_id, "<Button-1>", on_cell_click_factory(x,y,r_id, False))
+                canv.tag_bind(r_id, "<Button-3>", on_cell_click_factory(x,y,r_id, True))
                 grid[x][y].gid = r_id
         mode = "ORTHOGONAL"
     else: #ISOMETRIC
@@ -89,22 +138,26 @@ def generate_grid(which_mode):
                 x4 = x2
                 y4 = y1 + 0.5*dy
                 r_id = canv.create_polygon([x1,y1,x2,y2,x3,y3,x4,y4], fill = grid[x][y].get_gcol(),outline='black') 
-                canv.tag_bind(r_id, "<Button-1>", on_cell_click(x,y,r_id,1))
-                canv.tag_bind(r_id, "<Button-3>", on_cell_click(x,y,r_id,-1))
+                canv.tag_bind(r_id, "<Button-1>", on_cell_click_factory(x,y,r_id, False))
+                canv.tag_bind(r_id, "<Button-3>", on_cell_click_factory(x,y,r_id, True))
                 grid[x][y].gid = r_id
         mode = "ISOMETRIC"
     
+    for x in range(total_sz[0]):
+        for y in range(total_sz[1]):  
+            grid[x][y].fireImageScaleEvent()
+    
 def game_loop():
-    global canv, grid, keydowns, imgs
+    global canv, grid, keydowns, imgs, active_tile
     
     #calculate panning of the camera
     dx = keydowns[2] - keydowns[0]
     dy = keydowns[3] - keydowns[1]
     #actually pan the camera
     if dx:
-        canv.xview_scroll(dx, UNITS)
+        canv.xview_scroll(dx, tk.UNITS)
     if dy:
-        canv.yview_scroll(dy, UNITS)
+        canv.yview_scroll(dy, tk.UNITS)
 
     canv.update()
     canv.after(25, game_loop)
@@ -150,14 +203,14 @@ def center_screen():
         dx = cwidth/onscreen[0]
         canv["xscrollincrement"] = 0.5 * (total_sz[0] - onscreen[0]) * dx
         canv["yscrollincrement"] = 0.5 * (total_sz[1] - onscreen[1]) * dy
-        canv.xview_scroll(1, UNITS)
-        canv.yview_scroll(1, UNITS)
+        canv.xview_scroll(1, tk.UNITS)
+        canv.yview_scroll(1, tk.UNITS)
     else: #utter ridiculousness
         dx = cwidth/onscreen[0] * math.sqrt(2) #!
         canv["yscrollincrement"] = (cheight / 2) - (dy/2)
-        canv.yview_scroll(-1, UNITS)
+        canv.yview_scroll(-1, tk.UNITS)
         canv["xscrollincrement"] = 0.5 * (total_sz[0] - (onscreen[0]/math.sqrt(2))) * dx
-        canv.xview_scroll(1, UNITS)
+        canv.xview_scroll(1, tk.UNITS)
         
     
     canv["xscrollincrement"] = oldxs
@@ -170,10 +223,7 @@ def new():
     onscreen = [6,6]
     base_onscreen = onscreen
     total_sz = [10,10]
-    grid = [[Cell((x+y)%2,canv,x,y) for x in range(total_sz[0])] for y in range(total_sz[1])]
-    grid[4][4].color = 1
-    grid[4][5].color = 3
-    grid[5][5].color = 2
+    grid = [[Cell(canv,x,y,color=(x+y)%2) for x in range(total_sz[0])] for y in range(total_sz[1])]
     
     generate_grid(mode) #Make graphical board
 
@@ -214,7 +264,7 @@ def load_file():
         with f:
             _total_sz = list(map(int,f.readline().strip().split(",")))
             _onscreen = list(map(int,f.readline().strip().split(",")))
-            _grid     = [[Cell(0,canv,x,y) for x in range(_total_sz[0])] for y in range(_total_sz[1])]
+            _grid     = [[Cell(canv,x,y) for x in range(_total_sz[0])] for y in range(_total_sz[1])]
             for y in range(_total_sz[1]): #used up all my brainpower and forgot how to do this as a list comp
                 line_serial = f.readline().strip().split(",")
                 for x, elem in enumerate(line_serial):
@@ -277,12 +327,14 @@ def attempt_write(filename):
         return False
     return True
     
-def disp_cell_size_debug():
+def disp_cell_size_debug(pop):
     cwidth = canv.winfo_width()
     cheight = canv.winfo_height()
-    dx = (cwidth/onscreen[0] * math.sqrt(2)) if mode == "ISOMETRIC" else (cwidth/onscreen[0]) 
+    dx = ((cwidth/onscreen[0]) * math.sqrt(2)) if mode == "ISOMETRIC" else (cwidth/onscreen[0]) #WHAT
     dy = cheight/onscreen[1]
-    messagebox.showinfo('Info', str(round(dx,5)) + "," + str(round(dy,5)))
+    if pop:
+        messagebox.showinfo('Info', str(round(dx,5)) + "," + str(round(dy,5)))
+    return (dx,dy)
     
 def import_assets():
     global selector, imgs
@@ -292,8 +344,9 @@ def import_assets():
     raw_imgs = []
     for filename in filenames:
         try:
-            raw_imgs.append(tk.PhotoImage(file=filename))
-        except: #TODO: except WHAT?
+            raw_imgs.append( ImageTk.PhotoImage ( Image.open(filename) ) )
+        except Exception as e: #TODO: except WHAT?
+            print(e)
             errs.append(filename)
             
     if errs: #at least one image failed
@@ -303,15 +356,23 @@ def import_assets():
     off = len(imgs)
     #insert valid images and bind selectors:
     for i, img in enumerate(raw_imgs, start=off):
-        x = ((i % 3) * 66.66) + 32 + 10 
+        x = ((i % 3) * 66.66) + 32 + 10     #TODO: Modular scaling
         y = (int(i / 3) * (32+10)) + 16 + 6
         
         r_id = selector.create_image(x,y,image=img)
-        selector.tag_bind(r_id, "<Button-1>", lambda k: donothing)
-        selector.tag_bind(r_id, "<Button-3>", lambda k: donothing)
+        selector.tag_bind(r_id, "<Button-1>", lambda k: set_active_tile(r_id))
+        selector.tag_bind(r_id, "<Button-3>", lambda k: unset_active_tile())
         selector.tag_bind(r_id, "<Shift-Button-3>", destroy_img_factory(r_id))
         imgs[r_id] = img #store
     selector.update()
+        
+def set_active_tile(sel_id):
+    global active_tile, imgs
+    active_tile = imgs[sel_id]
+        
+def unset_active_tile():
+    global active_tile
+    active_tile = None
         
 def destroy_img_factory(tk_id): 
     def destr_img(ignore): #this is actually necessary because python's GC is overzealous
@@ -325,11 +386,16 @@ def destroy_img_factory(tk_id):
             x = ((i % 3) * 66.66) + 32 + 10 
             y = (int(i / 3) * (32+10)) + 16 + 6
             selector.coords(r_id, x, y)
+        unset_active_tile()
         selector.update()
     return destr_img
     
+def show_help():
+    string = "..."
+    messagebox.showinfo("Info",string)
+    
 if __name__ == "__main__":
-    s_width,s_height = 700,500
+    s_width,s_height = 750,525
     root = tk.Tk()
     ttk.Style(root).theme_use('alt')
     
@@ -346,16 +412,16 @@ if __name__ == "__main__":
     canv.update()
     
     #scrollbar for tiles
-    selector_bar = ttk.Scrollbar(root, orient=VERTICAL)
-    selector_bar.grid(row=0,column=0,sticky=(N,S))
+    selector_bar = ttk.Scrollbar(root, orient=tk.VERTICAL)
+    selector_bar.grid(row=0,column=0,sticky=(tk.N,tk.S))
     
     #tile 'selector'
-    selector = tk.Canvas(root, bg='white',yscrollcommand=selector_bar.set, height=500, width=200, borderwidth=5, relief='sunken')
+    selector = tk.Canvas(root,yscrollcommand=selector_bar.set, height=500, width=200, borderwidth=5, relief='sunken')
     selector.pack_propagate(0)
     selector.grid(row=0,column=1)
     
-    menubar = Menu(root)
-    filemenu = Menu(menubar, tearoff=0)
+    menubar = tk.Menu(root)
+    filemenu = tk.Menu(menubar, tearoff=0)
     filemenu.add_command(label="New", command=new, accelerator="Ctrl-N")
     root.bind_all("<Control-n>", lambda k: new()) #needed, as otherwise tries to pass new a param if just given raw FP
     filemenu.add_command(label="Open", command=load_file,accelerator="Ctrl-O")
@@ -371,7 +437,7 @@ if __name__ == "__main__":
     filemenu.add_command(label="Exit", command=root.quit)
     menubar.add_cascade(label="File", menu=filemenu)
 
-    viewmenu = Menu(menubar, tearoff=0)
+    viewmenu = tk.Menu(menubar, tearoff=0)
     viewmenu.add_command(label="Center",     command=center_screen,    accelerator="C"   )
     viewmenu.add_command(label="Zoom In",    command=lambda: zoom(-1), accelerator="Ctrl +")
     root.bind_all("<Control-plus>",  lambda k: zoom(-1))
@@ -381,24 +447,31 @@ if __name__ == "__main__":
     viewmenu.add_command(label="Reset Zoom", command=lambda: zoom(0),  accelerator="Ctrl+0")
     root.bind_all("<Control-0>",     lambda k: zoom(0))
     viewmenu.add_separator()
-    viewmenu.add_command(label="Orthogonal", command=lambda: generate_grid("ORTHOGONAL"), accelerator="Ctrl+Shift+O")
-    root.bind_all("<Control-Shift-O>", lambda k: generate_grid("ORTHOGONAL"))
-    viewmenu.add_command(label="Isometric",  command=lambda: generate_grid("ISOMETRIC"), accelerator="Ctrl+Shift+I")
-    root.bind_all("<Control-Shift-I>", lambda k: generate_grid("ISOMETRIC"))
+    #swapping requires messing with UI for tiles, not going to bother with for this
+    #viewmenu.add_command(label="Orthogonal", command=lambda: generate_grid("ORTHOGONAL"), accelerator="Ctrl+Shift+O")
+    #root.bind_all("<Control-Shift-O>", lambda k: generate_grid("ORTHOGONAL"))
+    #viewmenu.add_command(label="Isometric",  command=lambda: generate_grid("ISOMETRIC"), accelerator="Ctrl+Shift+I")
+    #root.bind_all("<Control-Shift-I>", lambda k: generate_grid("ISOMETRIC"))
     menubar.add_cascade(label="View", menu=viewmenu)
     
-    debugmenu = Menu(menubar, tearoff=0)
-    debugmenu.add_command(label="Cell Size", command=disp_cell_size_debug)
+    debugmenu = tk.Menu(menubar, tearoff=0)
+    debugmenu.add_command(label="Cell Size", command=lambda: disp_cell_size_debug(True))
     menubar.add_cascade(label="Debug", menu=debugmenu)
+
+    menubar.add_command(label="Help...", command=show_help)
 
     root.config(menu=menubar)
     
-    mode="ORTHOGONAL"
+    root.bind_all("<Escape>", lambda k: unset_active_tile())
+    
+    mode="ISOMETRIC"
     new() #make fresh board
     keydowns = [False for i in range(4)] #LEFT UP RIGHT DOWN
     
     saved = False #IO
     imgs = collections.OrderedDict()
+    active_tile = None
+    #root.bind("<Motion>", active_tile_micro_on_cursor)
     
     game_loop()
     root.mainloop()
